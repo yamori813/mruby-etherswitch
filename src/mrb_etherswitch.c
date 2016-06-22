846181725f97be4d72c9e321d6ec6a8280bd2b3a
Hiroki Mori <yamori813@yahoo.co.jp>
2016-06-22 13:06:16 +0900

    copy etherswitchcfg code

--- src/mrb_etherswitch.c
+++ src/mrb_etherswitch.c
@@ -10,11 +10,25 @@
 #include "mruby/data.h"
 #include "mrb_etherswitch.h"
 
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <net/if_media.h>
+#include <dev/etherswitch/etherswitch.h>
+
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
 typedef struct {
-  char *str;
-  int len;
+  int fd;
 } mrb_etherswitch_data;
 
 static const struct mrb_data_type mrb_etherswitch_data_type = {
@@ -24,35 +38,56 @@ static const struct mrb_data_type mrb_etherswitch_data_type = {
 static mrb_value mrb_etherswitch_init(mrb_state *mrb, mrb_value self)
 {
   mrb_etherswitch_data *data;
-  char *str;
-  int len;
+  char device[32];
+  mrb_int num;
 
-  data = (mrb_etherswitch_data *)DATA_PTR(self);
+  data = (mrb_bsdiic_data *)DATA_PTR(self);
   if (data) {
     mrb_free(mrb, data);
   }
-  DATA_TYPE(self) = &mrb_etherswitch_data_type;
+  DATA_TYPE(self) = &mrb_bsdiic_data_type;
   DATA_PTR(self) = NULL;
 
-  mrb_get_args(mrb, "s", &str, &len);
-  data = (mrb_etherswitch_data *)mrb_malloc(mrb, sizeof(mrb_etherswitch_data));
-  data->str = str;
-  data->len = len;
+  mrb_get_args(mrb, "i", &num);
+  data = (mrb_bsdiic_data *)mrb_malloc(mrb, sizeof(mrb_bsdiic_data));
+  snprintf(device, sizeof(device), "/dev/etherswitch%u", num);
+  data->fd = open(device, O_RDWR); 
   DATA_PTR(self) = data;
 
   return self;
 }
 
-static mrb_value mrb_etherswitch_hello(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_etherswitch_readreg(mrb_state *mrb, mrb_value self)
 {
-  mrb_etherswitch_data *data = DATA_PTR(self);
+  mrb_bsdiic_data *data = DATA_PTR(self);
+  mrb_int addr, reg;
+  struct etherswitch_reg er;
+  char cmdbuf = 0;
 
-  return mrb_str_new(mrb, data->str, data->len);
+  mrb_get_args(mrb, "i", &addr);
+
+  er.reg = r;
+  if (ioctl(data->fd, IOETHERSWITCHGETREG, &er) != 0)
+    return mrb_fixnum_value(-1);
+
+  return mrb_fixnum_value(cmdbuf);
 }
 
-static mrb_value mrb_etherswitch_hi(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_etherswitch_writereg(mrb_state *mrb, mrb_value self)
 {
-  return mrb_str_new_cstr(mrb, "hi!!");
+  mrb_bsdiic_data *data = DATA_PTR(self);
+  mrb_int addr, val;
+  struct etherswitch_reg er;
+
+  mrb_get_args(mrb, "ii", &addr, &val);
+
+  er.reg = addr;
+  er.val = val;
+
+  if (ioctl(data->fd, IOETHERSWITCHSETREG, &er) != 0)
+    return mrb_fixnum_value(-1);
+
+  return mrb_fixnum_value(0);
 }
 
 void mrb_mruby_etherswitch_gem_init(mrb_state *mrb)
@@ -60,8 +95,8 @@ void mrb_mruby_etherswitch_gem_init(mrb_state *mrb)
     struct RClass *etherswitch;
     etherswitch = mrb_define_class(mrb, "EtherSwitch", mrb->object_class);
     mrb_define_method(mrb, etherswitch, "initialize", mrb_etherswitch_init, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, etherswitch, "hello", mrb_etherswitch_hello, MRB_ARGS_NONE());
-    mrb_define_class_method(mrb, etherswitch, "hi", mrb_etherswitch_hi, MRB_ARGS_NONE());
+    mrb_define_method(mrb, bsdiic, "readreg", mrb_etherswitch_readreg, MRB_ARGS_REQ(1));
+    mrb_define_method(mrb, bsdiic, "writereg", mrb_etherswitch_writereg, MRB_ARGS_REQ(2));
     DONE;
 }
 
